a3a694ff41db1441b4f4eee6f1c8b2570c4c3853
Emanuel Nzinga Maimona <emanuelmaimona@gmail.com>
2022-08-15 09:07:57 +0200

    add base Models(User, Agent, Lead)

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from leads.models import Lead, Agent
+
+admin.site.register(Lead)
+admin.site.register(Agent)
 
 # Register your models here.
--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+#User = get_user_model()
+''' It is always advisable to create your customized user model
+     and not user the built-in get_user_model()
+'''
+class User(AbstractUser):
+    pass
+
+class Lead(models.Model):
+    # SOURCE_CHOICES = (
+    #     ('Youtube', 'Youtube'),
+    #     ('Google', 'Google'),
+    #     ('Facebook', 'Facebook'),
+    # )
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+    # phoned = models.BooleanField(default=False)
+    # source = models.CharField(choice=SOURCE_CHOICES, max_length=100)
+
+    # profile_picture = models.ImageField(blank=True, null=True)
+    # special_files = models.FileField(blank=True, null=True)
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+
