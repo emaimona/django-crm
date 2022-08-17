fe97477a8cc3fd23f371f6c92aeff2654a2e19da
Emanuel Nzinga Maimona <emanuelmaimona@gmail.com>
2022-08-18 00:05:06 +0200

    working link features on lead

--- leads/admin.py
+++ leads/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from leads.models import Lead, Agent
+from leads.models import Lead, Agent, User
 
+admin.site.register(User)
 admin.site.register(Lead)
 admin.site.register(Agent)
 
--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lead
 
 # Create your views here.
+def lead_list(request):
+    leads = Lead.objects.all()
+    return render(request, "leads/lead_list.html", context={'lead': leads})
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    return render(request, 'leads/lead_detail.html', {'lead': lead})
+
+def lead_create(request):
+
+    return render('leads/lead_create.html')
